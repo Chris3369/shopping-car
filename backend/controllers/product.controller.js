bfeafb4454edee1de4c7e920527ca835eec6a12d
Chris.T.Kuo <kuoaws@gmail.com>
2025-01-16 16:08:55 +0800

    toggleFeaturedProduct

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -2,6 +2,14 @@ import Product from '../models/product.model.js'
 import { redis } from '../lib/redis.js'
 import cloudinary from '../lib/cloudinary.js'
 
+const updateFeaturedProductsCache = async () => {
+    try {
+        const products = await Product.find({ isFeatured: true }).lean()
+        await redis.set('featured_products', JSON.stringify(products))
+    } catch (error) {
+    }
+}
+
 export const getAllProducts = async (req, res) => {
     try {
         const products = await Product.find() // find all products
@@ -76,6 +84,40 @@ export const deleteProduct = async (req, res) => {
 
         await Product.findByIdAndDelete(req.params.id)
         res.json({ message: 'Product deleted successfully' })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+export const getRecommendedProducts = async (req, res) => {
+
+}
+
+export const getProductsByCategory = async (req, res) => {
+    
+    const category = req.params.category
+    
+    try {
+        const products = await Product.find({ category })
+        res.json(products)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+export const toggleFeaturedProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        if (product) {
+            product.isFeatured = !product.isFeatured
+            const updatedProduct = await product.save()
+
+            await updateFeaturedProductsCache()
+            
+            res.json(updatedProduct)
+        } else{
+            res.status(404).json({ message: 'Product not found' })
+        }
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
