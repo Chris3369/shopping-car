9b9029b6a6be88cbcd599ed324bcaa1edaba44b3
Chris.T.Kuo <kuoaws@gmail.com>
2025-01-16 15:45:09 +0800

    cloudinary

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,10 +1,82 @@
 import Product from '../models/product.model.js'
+import { redis } from '../lib/redis.js'
+import cloudinary from '../lib/cloudinary.js'
 
-export const getProducts = async (req, res) => {
+export const getAllProducts = async (req, res) => {
     try {
         const products = await Product.find() // find all products
         res.json(products)
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
-}   
+}
+
+export const getFeaturedProducts = async (req, res) => {
+    try {
+        let products = await redis.get('featured_products')
+        if (products) {
+            return res.json(JSON.parse(products))
+        }
+
+        // if not in redis, get from db
+        // lean is used to convert to js object
+        products = await Product.find({ isFeatured: true }).lean()
+        if (!products) {
+            res.status(404).json({ message: 'No featured products found' })
+        }
+
+        // store in redis for future quick access
+        await redis.set('featured_products', JSON.stringify(products))
+        res.json(products)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+export const createProduct = async (req, res) => {
+    try {
+        const { name, description, price, category, image } = req.body
+
+        let cloudinaryResponse = null
+
+        if (image) {
+            cloudinaryResponse = await cloudinary.uploader.upload(image, { folder: 'products' })
+        }
+
+        const product = await Product.create({
+            name,
+            description,
+            price,
+            category,
+            image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : ""
+        })
+
+        res.status(201).json(product)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' })
+        }
+
+        if (product.image) {
+            const imageId = product.image.split('/').pop().split('.')[0]
+
+            try {
+                await cloudinary.uploader.destroy(`products/${imageId}`)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        await Product.findByIdAndDelete(req.params.id)
+        res.json({ message: 'Product deleted successfully' })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import { getProducts } from '../controllers/product.controller.js'
+import { getAllProducts, getFeaturedProducts, createProduct, deleteProduct } from '../controllers/product.controller.js'
 import { protectRoute, adminRoute } from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
-router.get('/', protectRoute, adminRoute, getProducts)
+router.get('/', protectRoute, adminRoute, getAllProducts)
+router.get('/featured', getFeaturedProducts)
+router.post('/', protectRoute, adminRoute, createProduct)
+router.delete('/:id', protectRoute, adminRoute, deleteProduct)
+
 
 export default router
